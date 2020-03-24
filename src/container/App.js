0885748b696db2d81c94dd0313cc2e9102aab9ed
Meadhavi <meadhavi@Meadhavis-MacBook-Pro.local>
2020-03-24 14:37:49 +0530

    Added headerComponent and linking to homepage, ShopPage, contactUs page

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Header from '../components/header/header.component';
 
 import Homepage from '../pages/homepage/homepage.component';
 import ShopPage from '../pages/shop/shop.component';
+
+const ContactUs = () => (
+  <h1>Contact Us</h1>
+);
 
 const PageNotFound = () => (
   <h1>Page Not Found</h1>
@@ -12,10 +17,12 @@ const PageNotFound = () => (
 class App extends Component {
   render() {
     return (
-      <div>  
+      <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route  exact path="/shop" component={ShopPage} />
+          <Route exact path="/contact" component={ContactUs} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
