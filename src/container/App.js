1db5207d6d0d3233ed804fb0b4677de605e83b31
Meadhavi <meadhavi@Meadhavis-MacBook-Pro.local>
2020-03-24 12:57:58 +0530

    Updating our app with the shoppage -- CollectionPreview && CollectionItem Component

--- src/container/App.js
+++ src/container/App.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Homepage from '../pages/homepage/homepage.component';
-
-const HatsPage = (props) => {
-  console.log('hats page props: ', props.match);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-      <Link to={`${props.match.url}/1 `}> Topic 1 </Link>
-      <Link to={`${props.match.url}/2 `}>Topic 2 </Link>
-    </div>
-  )
-}
-
-const HatsDetail = (props) => {
-  console.log('Props', props);
-  return (
-   <React.Fragment>
-    <h1>Hats Details Page { props.match.params.hatsId }</h1>
-   </React.Fragment>
-  )
-}
+import ShopPage from '../pages/shop/shop.component';
 
 const PageNotFound = () => (
   <h1>Page Not Found</h1>
@@ -34,8 +15,7 @@ class App extends Component {
       <div>  
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route  exact path="/hats" component={HatsPage} />
-          <Route  path="/hats/:hatsId" component={HatsDetail} />
+          <Route  exact path="/shop" component={ShopPage} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
-import { Link } from 'react-router-dom';
-
 import Directory from '../../components/directory/directory.component';
 
 const Homepage = (props) => {
