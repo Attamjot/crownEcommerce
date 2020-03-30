8898bd322550f7513d5704388de27915f39e349e
Meadhavi <meadhavi@Meadhavis-MacBook-Pro.local>
2020-03-30 13:34:33 +0530

    Updating our app with reselect library and also updated the selectors with reselect ( memoized ) selectors.

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import CustomButon from '../custom-button/custom-button.component';
-
+import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className="cart-dropdown">
-        <div className="cart-items"></div>
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} /> )
+            }
+        </div>
         <CustomButon>Go to Checkout Page</CustomButon>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => {
+    console.log('Cart Dashboard being called');
+    return {
+      cartItems: selectCartItems(state)
+    };
+}
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -3,16 +3,24 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 import {connect} from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemsCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count"> {itemsCount} </span>
     </div>
 );
+
+const mapStateToProps = state => {
+    console.log("CartIcon: I'm being called");
+    return {
+      itemsCount: selectCartItemsCount(state)
+    };
+}
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
